233d11953945c57b11f1d872262c69970ad8342f
alpjamal <mr.abdujamol1@mail.ru>
2023-08-22 16:12:48 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ulesson_auth_firebase/auth/presentation/login_page.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
